db4b621bbaa0600cfcba670f09fd41a21f29dab6
dipoengoro <dipoengoro@outlook.com>
2022-01-25 12:04:29 +0700

    Pass Challenge 8

--- app.js
+++ app.js
@@ -47,6 +47,12 @@ app.get('/compose', (req, res) => {
   res.render('compose', data)
 })
 
+app.post('/compose', (req, res) => {
+  const item = req.body.newItem
+  console.log(item)
+  res.redirect('/compose')
+})
+
 app.listen(PORT, function() {
   console.log("Server started on port " + PORT)
 })
